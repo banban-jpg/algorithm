709495476cfb708e972d7f76598fc5c0e5369fd9
banban-jpg <qkswnsdud@gmail.com>
2025-10-18 11:51:38 +0900

    [Silver I] Title: 경로 찾기, Time: 160 ms, Memory: 16588 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 경로 확인
+        for (int k = 0; k < n; k++) {       
+            for (int i = 0; i < n; i++) {   
+                for (int j = 0; j < n; j++) { 
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(graph[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
