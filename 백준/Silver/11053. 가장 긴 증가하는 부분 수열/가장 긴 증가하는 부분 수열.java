1b34979b84cb14a96b4ae9fb43509d026aa3203b
banban-jpg <qkswnsdud@gmail.com>
2025-11-01 20:00:01 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 14812 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(getLISLength(arr));
+    }
+
+    private static int getLISLength(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        int maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1; // 자신으로 시작
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+}
