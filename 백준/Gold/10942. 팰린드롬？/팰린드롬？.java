e92438330de3e671ab85dd43d8f4e6d3c848208d
banban-jpg <qkswnsdud@gmail.com>
2026-02-23 14:16:35 +0900

    [Gold IV] Title: 팰린드롬?, Time: 644 ms, Memory: 217772 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] dp = new boolean[N + 1][N + 1];
+
+        // 길이 1
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = true;
+        }
+
+        // 길이 2
+        for (int i = 1; i < N; i++) {
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
+        }
+
+        // 길이 3 이상
+        for (int len = 3; len <= N; len++) {
+            for (int i = 1; i + len - 1 <= N; i++) {
+                int j = i + len - 1;
+
+                if (arr[i] == arr[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            if (dp[S][E]) sb.append("1\n");
+            else sb.append("0\n");
+        }
+
+        System.out.print(sb);
+    }
+}
